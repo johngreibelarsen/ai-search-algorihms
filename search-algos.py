0759852b4aea3bb2a7a1193a7644ae0d3adcb9f5
John Greibe Larsen <johnglarsen@hotmail.co.uk>
2021-03-15 23:02:24 +0000

    Added new Corner Search Problem and solution

--- search-algos.py
+++ search-algos.py
@@ -258,3 +258,103 @@ class PositionSearchProblem(search.SearchProblem):
             if self.walls[x][y]: return 999999
             cost += self.costFn((x,y))
         return cost
+
+
+class CornersProblem(search.SearchProblem):
+    """
+    This search problem finds paths through all four corners of a layout.
+
+    You must select a suitable state space and successor function
+    """
+
+    def __init__(self, gameState, costFn=lambda x: 1, start=None, warn=True, visualize=True):
+        """
+        Stores the walls, pacman's starting position and corners.
+        """
+        self.walls = gameState.getWalls()
+        self.startingPosition = gameState.getPacmanPosition()
+        top, right = self.walls.height - 2, self.walls.width - 2
+        self.corners = ((1, 1), (1, top), (right, 1), (right, top))
+        for corner in self.corners:
+            if not gameState.hasFood(*corner):
+                print('Warning: no food in corner ' + str(corner))
+
+        self.visualize = visualize
+        if warn and (gameState.getNumFood() != 1):
+            print('Warning: this does not look like a regular search maze')
+
+        # For display purposes
+        self._visited, self._visitedlist, self._expanded = {}, [], 0  # DO NOT CHANGE
+
+    def getStartState(self):
+        """
+        Returns the start state (in your state space, not the full Pacman state
+        space)
+        """
+        return (self.startingPosition, tuple())
+
+    def isGoalState(self, state):
+        """
+        Returns whether this search state is a goal state of the problem.
+        """
+        isGoal = False
+        pos = state[0]
+        visited_corners = list(state[1])
+        if pos in self.corners:
+            if pos not in visited_corners:
+                visited_corners.append(pos)
+                state[1] = tuple(visited_corners)
+            isGoal = len(visited_corners) == len(self.corners)
+
+        # For display purposes only
+        if isGoal and self.visualize:
+            self._visitedlist.append(state[0])
+            import __main__
+            if '_display' in dir(__main__):
+                if 'drawExpandedCells' in dir(__main__._display):  # @UndefinedVariable
+                    __main__._display.drawExpandedCells(self._visitedlist)  # @UndefinedVariable
+
+        return isGoal
+
+    def getSuccessors(self, state):
+        """
+        Returns successor states, the actions they require, and a cost of 1.
+         As noted in search.py:
+            For a given state, this should return a list of triples, (successor,
+            action, stepCost), where 'successor' is a successor to the current
+            state, 'action' is the action required to get there, and 'stepCost'
+            is the incremental cost of expanding to that successor
+        """
+        x, y = state[0]
+        visited_corners = list(state[1])
+        successors = []
+        for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            next_node = (nextx, nexty)
+            hitsWall = self.walls[nextx][nexty]
+            if not hitsWall:
+                sucVCorners = list(visited_corners)
+                if next_node in self.corners:
+                    if next_node not in sucVCorners:
+                        sucVCorners.append(next_node)
+                successor = ((next_node, tuple(sucVCorners)), action, 1)
+                successors.append(successor)
+
+        # Bookkeeping for display purposes
+        self._expanded += 1  # DO NOT CHANGE
+        return successors
+
+
+    def getCostOfActions(self, actions):
+        """
+        Returns the cost of a particular sequence of actions.  If those actions
+        include an illegal move, return 999999.  This is implemented for you.
+        """
+        if actions == None: return 999999
+        x, y = self.startingPosition
+        for action in actions:
+            dx, dy = Actions.directionToVector(action)
+            x, y = int(x + dx), int(y + dy)
+            if self.walls[x][y]: return 999999
+        return len(actions)
