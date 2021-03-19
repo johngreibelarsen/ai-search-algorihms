5fea0df971c51a52a2720d19a4e1e139b87c6871
John Greibe Larsen <johnglarsen@hotmail.co.uk>
2021-03-19 00:10:01 +0000

    Added a maze distance function

--- search-algos.py
+++ search-algos.py
@@ -673,3 +673,22 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         """
         return state in self.food.asList()
 
+
+def mazeDistance(point1, point2, gameState):
+    """
+    Returns the maze distance between any two points, using the search functions
+    you have already built. The gameState can be any game state -- Pacman's
+    position in that state is ignored.
+
+    Example usage: mazeDistance( (2,4), (5,6), gameState)
+
+    This might be a useful helper function for your ApproximateSearchAgent.
+    """
+    x1, y1 = point1
+    x2, y2 = point2
+    walls = gameState.getWalls()
+    assert not walls[x1][y1], 'point1 is a wall: ' + str(point1)
+    assert not walls[x2][y2], 'point2 is a wall: ' + str(point2)
+    prob = PositionSearchProblem(gameState, start=point1, goal=point2, warn=False, visualize=False)
+    path = search.bfs(prob)
+    return (len(path), path)
